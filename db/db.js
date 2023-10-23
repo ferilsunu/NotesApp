07b29d17ca5f321e5229127e44f412352d952a3d
Feril Sunu <ferilsunu@gmail.com>
2023-10-24 03:35:00 +0400

    feat: enhance layout navigation, user profile card, and footer panels

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mongoose= require('mongoose')
 
 const connection = mongoose.connect(process.env.DB_URL,{useNewUrlParser: true,useUnifiedTopology: true}).then(()=>{
     console.log("Database Connected")
-}).catch(error=>console.log(error))
+}).catch(error=>console.log("database connection failed",error))
 
 module.exports = connection
 
