8af5f19cce0b805e24e0e049a41e351b82c23d8a
feril <ferilsunu@gmail.com>
2026-09-20 10:50:09 +0400

    fix: remove intrusive flash error on unauthenticated login redirect

--- config/authChecker.js
+++ config/authChecker.js
@@ -4,7 +4,6 @@ module.exports = {
             //req.isAuthenticated() will return true if user is logged in
             next();
         } else{
-            req.flash('error','please sign to use the app')
             res.redirect("/login");
         }
     }
